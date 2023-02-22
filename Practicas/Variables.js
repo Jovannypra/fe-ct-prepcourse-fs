2304c3b236bb7a60cb5202980443216c1569ae84
Jovanny Jose Prado gonzalez <Jovannyrenaware@gmail.com>
2023-02-21 22:54:21 -0400

    prueba

---
+++ Practicas/Variables.js
@@ -0,0 +1,32 @@
+var cajon1;
+// DEFINIENDO UNA VARIABLE Y ASIGNAMOS VALOR
+// Variables : Son los contenedores
+// asignarle valor
+var cajonUno = "guantes";
+
+//tipos de datos
+//string : representa datos textuales (cadenas de caracteres)
+var nombre = "Jovanny";
+
+//Numeros : Permite representar manipular valores numericos + - decimales
+var nroEntero = 17;
+var nroNegativo = -17;
+var nroDecimal = 11.75;
+
+//boleanos : representa un valor logico y puede tener dos valores,  True o False.
+var estoyEntendiendo = true;
+var estoyAburrido = false;
+
+//undefined : Es una variable que no ha sido declarada o la cual no se le ha asignado un valor
+var cajon;
+console.log(cajon);
+
+//null: representa la ausencia intencional de cualquier valor,
+var sinNada = null;
+
+/* MÉTODO LENGTH: Este es un método que solo se puede aplicar al tipo de dato string 
+(más adelante veremos que también lo podemos utilizar en arreglos). En este ejemplo 
+podemos ver que si lo intentamos aplicar en un número o en un booleano, nos devuelve error.*/
+
+var string = "String";
+console.log(string.length); //R = 6
